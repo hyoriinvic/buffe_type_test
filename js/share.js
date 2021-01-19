601c715ee8890e131bee89f31e1725c638362ecb
hyoriinvic <hyoriinvic@gmail.com>
2021-01-19 19:58:27 +0900

    commit

--- js/share.js
+++ js/share.js
@@ -8,21 +8,21 @@ const fb = () => {
     'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
   return false;
 }
-const tw = () => {
-  let name = document.querySelector('.result').innerHTML;
-  switch (name) {
-    case '표범':
-    case '유니콘':
-      name += '이에요! ';
-      break;
-    default:
-      name += '예요! ';
-  }
-  window.open('https://twitter.com/intent/tweet?text='
-    + title + '%0A' + '저는 ' + name + '' + hash + '%0A' + url,
-    'twittersharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
-  return false;
-}
+// const tw = () => {
+//   let name = document.querySelector('.result').innerHTML;
+//   switch (name) {
+//     case '표범':
+//     case '유니콘':
+//       name += '이에요! ';
+//       break;
+//     default:
+//       name += '예요! ';
+//   }
+//   window.open('https://twitter.com/intent/tweet?text='
+//     + title + '%0A' + '저는 ' + name + '' + hash + '%0A' + url,
+//     'twittersharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+//   return false;
+// }
 const nv = () => {
   window.open('http://share.naver.com/web/shareView.nhn?url='
     + url + '&title=' + title,
